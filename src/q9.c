1e81f394fbc17984a9c4672149e03fc2cfc3c2df
hridduuu <shahhridaya39@gmail.com>
2025-09-06 21:11:30 +0545

    assignment completed

--- src/q9.c
+++ src/q9.c
@@ -5,3 +5,18 @@
     **
     *
 */
+#include <stdio.h>
+int main() {
+    int n = 5; 
+    int i = n;
+    while(i >= 1) {
+        int j = 1;
+        while(j <= i) {
+            printf("*");
+            j++;
+        }
+        printf("\n");
+        i--;
+    }
+    return 0;
+}
